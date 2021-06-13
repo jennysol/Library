baf982dffa0337d5a9343d5e38a694b35fbd4dda
jennysol <jennyssoliver@gmail.com>
2021-06-13 16:28:44 -0300

    Update: descending presentation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
     def index
-        @products = Product.order :name
+        @products = Product.order(name: :desc).limit 2
         @discounted_product = Product.order(:preco).limit 1
     end
 end
