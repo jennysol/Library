7dfa9ac880c8f39d573a569064246f3546bc5418
jennysol <jennyssoliver@gmail.com>
2021-06-13 14:18:14 -0300

    Feature: Add Products Model

---
+++ db/migrate/20210613171359_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :value
+      t.integer :amount
+      
+      t.timestamps
+    end
+  end
+end
