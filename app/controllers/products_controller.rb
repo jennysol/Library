a15038c2f163795647ce0388b5c05f69f141255d
jennysol <jennyssoliver@gmail.com>
2021-06-15 20:36:44 -0300

    Feature: save and redirect values

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
     def index
-        @products = Product.order(name: :desc).limit 2
+        @products = Product.order(name: :desc).limit 5
         @discounted_product = Product.order(:preco).limit 1
     end
 
     def create
-       product =  params.require(:product).permit(:name, :description, :preco, :amount)
+       product =  params.require(:product).permit(:name, :description, :value, :amount)
        Product.create product
+       redirect_to root_path
     end
 end
