104b5e49d51968229e2043aa07906050314508e5
jennysol <jennyssoliver@gmail.com>
2021-06-15 20:23:33 -0300

    Feature: add method create Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
         @products = Product.order(name: :desc).limit 2
         @discounted_product = Product.order(:preco).limit 1
     end
+
+    def create
+       product =  params.require(:product).permit(:name, :description, :preco, :amount)
+       Product.create product
+    end
 end
