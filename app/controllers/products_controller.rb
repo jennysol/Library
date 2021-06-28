943519575598c5e1a9109474ba78dcd951980838
jennysol <jennyssoliver@gmail.com>
2021-06-28 07:31:44 -0300

    Feature: Creating search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
     def index
-        @products = Product.order(name: :desc).limit 5
+        @products = Product.order(name: :asc).limit 5
         @discounted_product = Product.order(:preco).limit 1
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "products/search", to: "products#search", as: :search_product
   resources :products, only: [:new, :create, :destroy]
   root to: "products#index"
 end
